073e54fd67740630c0ddb12cea547fd9a6a69dda
sadil <sadil@vizuamatix.com>
2026-03-08 07:53:53 +0530

    Bucket frontend configs

--- next.config.js
+++ next.config.js
@@ -4,6 +4,18 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.supabase.co',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      }
+    ],
+  },
 };
 
 
